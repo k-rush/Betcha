b0f590f73f1b03324d9794df258203457632a194
jackfjo3 <jackfjo3@gmail.com>
2015-06-19 14:54:57 -0600

    installed simple_form

--- Gemfile
+++ Gemfile
@@ -11,11 +11,11 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-gem 'haml', '~> 4.0.6'
+gem 'haml', '~> 4.0.6' 
 gem 'haml-rails', '~> 0.9.0'
 gem 'simple_form', '~> 3.1.0'
 gem 'bootstrap-sass', '~> 3.3.4.1'
-gem 'devise', '~> 3.4.1'
+gem 'devise', '~> 3.4.1' # SPECIAL INSTALL STEPS REQUIRED 
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,4 @@ ActiveRecord::Schema.define(version: 20150618224508) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
 end
