7fc593125c65745a85a93156c40904ec4957ff79
jackfjo3 <jackfjo3@gmail.com>
2015-06-20 13:19:40 -0600

    generated Friendships controller and added create, update, and destroy friendship actions

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,34 @@
+class FriendshipsController < ApplicationController
+
+  def create
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id], accepted: false)
+    
+    if @friendship.save
+      flash[:notice] = "Friend request sent!"
+      redirect_to :back
+    else
+      flash[:error] = "Unable to send friend request"
+      redirect_to :back
+    end
+
+  end
+
+  def update
+    @friendship = Friendship.where(friend_id: current_user, user_id: params[:id]).first
+    @friendship.update(accepted: true)
+    
+    if @friendship.save
+      redirect_to root_url, notice: "Successfully added new friend!"
+    else
+      redirect_to root_url, notice: "Something went wrong, friend not added"
+    end
+
+  end
+
+  def destroy
+    @friendship = Friendship.where(friend_id: [current_user, params[:id]]).where(user_id: [current_user, params[:id]]).last
+    @friendship.destroy
+    flash[:notice] = "Removed friendship"
+    redirect_to :back
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'bets#index'
   devise_for :users
   resources :bets
+  resources :friendship #not sure if this is correct yet.
   
 end
