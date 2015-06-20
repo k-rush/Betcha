c7043f2ff5f9a3233105043477b123ec6429e69e
jackfjo3 <jackfjo3@gmail.com>
2015-06-20 13:48:41 -0600

    added user profile (UsersController with show action). Updated navbar with profile link as well as bets link (styling needs to be fixed)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  
+
   root 'bets#index'
   devise_for :users
+  resources :users, only: [:show]
   resources :bets
   resources :friendship #not sure if this is correct yet.
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619205608) do
+ActiveRecord::Schema.define(version: 20150620174817) do
 
   create_table "bets", force: :cascade do |t|
     t.text     "bet"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150619205608) do
     t.integer  "wager"
     t.integer  "odds"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.boolean  "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
